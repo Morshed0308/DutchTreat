c2bbfea8f6b2fb7b552de73ce1d6b3c695b64cb3
Shahriar <morshedshahriar1@gmail.com>
2018-11-26 01:06:42 +0600

    DutchRepo

---
+++ DutchTreat/DutchTreat/Data/DutchRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace DutchTreat.Data
+{
+    public class DutchRepository
+    {
+        private readonly DutchContext _ctx;
+        public DutchRepository(DutchContext ctx )
+        {
+            _ctx = ctx;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _ctx.Products
+                       .OrderBy(p => p.Title)
+                       .ToList();
+        }
+        public IEnumerable<Product>GetProductsByCategory(string category)
+        {
+            return _ctx.Products
+                       .Where(p => p.Category == category)
+                       .ToList();
+        }
+    }
+}
